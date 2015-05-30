e585cc40b787f13e07da1a2db791e2ce077f79ea
PANKAJ KUMAR <pknit1291@gmail.com>
2015-05-30 11:36:46 +0530

    Update and rename Rotate_by_N.py to Rotate_by_N_alt.py

--- Rotate_by_N.py
+++
@@ -1,6 +0,0 @@
-arr = map(int , raw_input().split())
-k = int(raw_input())
-l = len(arr)
-reverse_arr = arr[::-1]
-A ,B  = reverse_arr[:k] , reverse_arr[k:l]
-print A[::-1] + B[::-1]
---
+++ Rotate_by_N_alt.py
@@ -0,0 +1,5 @@
+import collections
+d = collections.deque([1 , 2 , 3 , 4 , 5 , 6 , 7])
+d.rotate(2)
+print list(d)
+
