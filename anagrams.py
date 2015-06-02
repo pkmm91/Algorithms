389a02767ec1a4a7ca6d3d7cbeff6f0f46de25d8
PANKAJ KUMAR <pknit1291@gmail.com>
2015-06-02 14:37:53 +0530

    Anagrams
    use of recursion to find anagrams

---
+++ anagrams.py
@@ -0,0 +1,11 @@
+def anagrams(s):
+    if s == '':
+        return [s]
+    else:
+        ans = []
+        for w in anagrams(s[1:]):
+            print w
+            for i in range(len(w)+1):
+                ans.append(w[:i] + s[0] + w[i:])
+        return ans
+print anagrams('hel')
